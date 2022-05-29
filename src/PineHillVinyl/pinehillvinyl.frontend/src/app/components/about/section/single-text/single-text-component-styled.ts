ee0fe9df8d64a5ac60eebb850007ba0d972bc318
Tine Franchois <tine.franchois@student.howest.be>
2022-05-29 10:55:50 +0200

    Add single text component

---
+++ src/PineHillVinyl/pinehillvinyl.frontend/src/app/components/about/section/single-text/single-text-component-styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/macro'
+
+interface Props{
+    contentDirection: string
+}
+
+
+export const Section = styled.section<Props>`
+
+`
+
+export const Title = styled.h1`
+
+`
+
+export const Message = styled.div`
+
+`
